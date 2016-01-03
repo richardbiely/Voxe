bcc8c0e094af478695c7a3fc4cfe3b8e7b05e400
bielyr@outlook.com <bielyr@outlook.com>
2016-01-03 07:42:55 +0100

    [Update] When generating world use 1D index directly instead of computing it from 3D all the time

--- Assets/Engine/Scripts/Generators/SimplePerlinGenerator.cs
+++ Assets/Engine/Scripts/Generators/SimplePerlinGenerator.cs
@@ -15,23 +15,24 @@ namespace Assets.Engine.Scripts.Generators
         #region IChunkGenerator implementation
 		public void Generate (Chunk chunk)
         {
+            int index = 0;
 			for (int y = 0; y < EngineSettings.ChunkConfig.SizeYTotal; y++)
 			{
 	            for (int z = 0; z < EngineSettings.ChunkConfig.SizeZ; z++)
 	            {
 					int wz = z + (chunk.Pos.Z << EngineSettings.ChunkConfig.LogSizeZ);
 
-	                for (int x = 0; x < EngineSettings.ChunkConfig.SizeX; x++)
+	                for (int x = 0; x < EngineSettings.ChunkConfig.SizeX; x++, index++)
 	                {
 						int wx = x + (chunk.Pos.X << EngineSettings.ChunkConfig.LogSizeX);
 
 	                    if (m_noise.GetValue(new Vector3(wx, y, wz) * 0.1f) > 0f)
 	                    {
-							chunk[x, y, z] = new BlockData (BlockType.Dirt);
+							chunk[index] = new BlockData (BlockType.Dirt);
 	                    }
 						else
 						{
-							chunk[x, y, z] = BlockData.Air;
+							chunk[index] = BlockData.Air;
 	                    }
 	                }
                 }
--- Assets/Engine/Scripts/Generators/Terrain/SimpleTerrainGenerator.cs
+++ Assets/Engine/Scripts/Generators/Terrain/SimpleTerrainGenerator.cs
@@ -15,13 +15,14 @@ namespace Assets.Engine.Scripts.Generators.Terrain
         #region IChunkGenerator implementation
         public void Generate (Chunk chunk)
         {
+            int index = 0;
 			for (int y = 0; y < EngineSettings.ChunkConfig.SizeYTotal; y++)
 			{
 	            for (int z = 0; z < EngineSettings.ChunkConfig.SizeZ; z++)
 	            {
 					int wz = z + (chunk.Pos.Z << EngineSettings.ChunkConfig.LogSizeZ);
 
-                    for (int x = 0; x < EngineSettings.ChunkConfig.SizeX; x++)
+                    for (int x = 0; x < EngineSettings.ChunkConfig.SizeX; x++, index++)
                     {
 						int wx = x + (chunk.Pos.X << EngineSettings.ChunkConfig.LogSizeX);
 
@@ -32,7 +33,7 @@ namespace Assets.Engine.Scripts.Generators.Terrain
 						{
                             if (!up)
 							{
-								chunk[x, y, z] = new BlockData(BlockType.Grass);
+								chunk[index] = new BlockData(BlockType.Grass);
                                 // Grass block was placed, lets place a tree blueprint here
                                 if (y < EngineSettings.ChunkConfig.SizeYTotal)
                                 {
@@ -42,14 +43,14 @@ namespace Assets.Engine.Scripts.Generators.Terrain
 							else
 							{
                                 if (y > 50)
-									chunk[x, y, z] = new BlockData(BlockType.Dirt);
+									chunk[index] = new BlockData(BlockType.Dirt);
                                 else
-									chunk[x, y, z] = new BlockData(BlockType.Stone);
+									chunk[index] = new BlockData(BlockType.Stone);
                             }
                         }
 						else
 						{
-							chunk[x, y, z] = BlockData.Air;
+							chunk[index] = BlockData.Air;
                         }
                     }
                 }
